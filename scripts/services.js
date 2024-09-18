b211ddc959ec89dad6ba53477b97260f2512d560
Jeff-GT <jefferson.guerrero@sdgku.edu>
2024-09-18 15:49:38 -0700

    commit #5 - 105 Assignment #2

--- scripts/services.js
+++ scripts/services.js
@@ -1,10 +1,55 @@
-function addService(){
-  let serviceNew=$("#txtService").val();
-  let priceNew=$("#numPrice").val();
-  $("#serviceAdded").append(`<div>${serviceNew}</div><div>${priceNew}</div>`);
+//contructor
+function Service(description,price){
+  this.description=description;
+  this.price=price;
 }
 
 
+function addService(){
+  let counter=0;
+  let serviceNew=$("#txtService").val();
+  let priceNew=$("#numPrice").val();
+
+  if(serviceNew==''){
+    
+    $("#txtService").addClass(`empty-form`);
+
+    
+    
+    counter++;
+  }
+  
+  if(priceNew==''){
+    
+    $("#numPrice").addClass(`empty-form`);
+    
+    
+    
+    counter++;
+  }
+
+  
+
+  if(counter!=0){
+    alert("Please complete the Form");
+    if(priceNew!=''){
+      $("#numPrice").removeClass(`empty-form`);
+    }
+    if(serviceNew!=''){
+      $("#txtService").removeClass(`empty-form`);
+    }
+  }
+  else{
+    let newService= new Service(serviceNew,priceNew);
+    $("#serviceAdded").append(`
+      <div>${serviceNew}</div>
+      <div>${priceNew}</div>`);
+      save(newService);
+      $("input").val("");
+      $("input").removeClass(`empty-form`);
+  }
+
+}
 
 
 function init() {
